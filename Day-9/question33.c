ac910ab1edd8b494e1807c5c4f33116fe03684f0
Vaibhav Gupta <gupta.123vaibhav123@gmail.com>
2026-06-06 23:09:30 +0530

    DAY-9 ADDED

---
+++ Day-9/question33.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    printf("the no of rows:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+        for(j=i;j>=1;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
